864c101252781321bd7eb1709475afa81dbf51aa
ParkYoungDuk <dydejrdms@naver.com>
2025-05-23 11:40:23 +0900

    create Project

---
+++ src/main/java/com/toy/myroomnow/MyroomnowApplication.java
@@ -0,0 +1,13 @@
+package com.toy.myroomnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyroomnowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyroomnowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toy/myroomnow/MyroomnowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.myroomnow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyroomnowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
